082b6eeb9cb0f10a1ef0ce217124ca3f19e9b48b
Sainvi Jaiswal <sainvijaiswal05@gmail.com>
2026-08-09 19:59:02 +0530

    Time: 71 ms (88.57%), Space: 26.1 MB (49.15%) - LeetHub

---
+++ 1140-stone-game-ii/1140-stone-game-ii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def stoneGameII(self, piles: List[int]) -> int:
+        for i in range(len(piles) -2, -1, -1):
+            piles[i] += piles[i+1]
+        @cache
+        def dfs(i, m):
+            if i+m*2 >= len(piles):
+                return piles[i]
+
+            return piles[i] - min(dfs(i+j, max(m,j)) for j in range(1, m*2+1))
+        return dfs(0,1) 
